b3a16ce10ffef3eaa60cfec71d34a2ac8b79d13e
kimdaeyeub <kdy3915@naver.com>
2024-02-08 01:34:29 +0900

    unshift, get 메서드 구현

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -56,11 +56,46 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count, current;
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (index !== count) {
+        count--;
+        current = current.prev;
+      }
+    }
+    return current;
+  }
 }
 
 const list = new DoublyLinkedList();
 list.push("asdasd");
 list.push("hello");
 list.push("Bye");
-list.shift();
+console.log(list.get(1));
 console.log(list);
